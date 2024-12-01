95bc5b3b94e81207786ad7aa1f39cc8524040d6f
francieltipantuna <franciel.tipantuna@epn.edu.ec>
2024-11-30 23:32:01 -0500

    The following classes were implemented: ExhaustPipe, Lights and Tire, with their respective attributes, constructors and getters and setters

--- src/Light.java
+++ src/Light.java
@@ -1,2 +1,49 @@
 public class Light {
+
+    private int lightsId;
+    private int lightStatus;
+    private int lightIntensity;
+    private String lightCoverStatus;
+
+    public Light(int lightsId, int lightStatus, int lightIntensity, String lightCoverStatus){
+
+        this.lightsId = lightsId;
+        this.lightStatus = lightStatus;
+        this.lightIntensity = lightIntensity;
+        this.lightCoverStatus = lightCoverStatus;
+
+    }
+
+    public int getLightsId() {
+        return lightsId;
+    }
+
+    public void setLightsId(int lightsId) {
+        this.lightsId = lightsId;
+    }
+
+    public int getLightStatus() {
+        return lightStatus;
+    }
+
+    public void setLightStatus(int lightStatus) {
+        this.lightStatus = lightStatus;
+    }
+
+    public int getLightIntensity() {
+        return lightIntensity;
+    }
+
+    public void setLightIntensity(int lightIntensity) {
+        this.lightIntensity = lightIntensity;
+    }
+
+    public String getLightCoverStatus() {
+        return lightCoverStatus;
+    }
+
+    public void setLightCoverStatus(String lightCoverStatus) {
+        this.lightCoverStatus = lightCoverStatus;
+    }
+
 }
